baab6369e0a4be37855f07e868479e0a748e8e19
carlos <antonio.carlos@morgancs.com.br>
2021-06-16 21:37:10 -0300

    update intentName

--- src/controllers/testeController.js
+++ src/controllers/testeController.js
@@ -4,13 +4,47 @@ module.exports = {
      return response.json("Teste")
    },
 
-   fulfillmentText(request, response){
+  //  fulfillmentText(request, response){
+  //    var intentName = request.body.queryResult.intent.displayName;
+
+  //    if(intentName === 'processo.seletivo'){
+  //     response.json({"fulfillmentText": "Processo seletivo"});
+  //    }else if(intentName === 'atendimento.horario'){
+  //     response.json({"fulfillmentText": "Horario de atendimento"});
+  //    }
+  //  }
+
+     fulfillmentText(request, response){
      var intentName = request.body.queryResult.intent.displayName;
 
      if(intentName === 'processo.seletivo'){
-      response.json({"fulfillmentText": "Processo seletivo"});
-     }else if(intentName === 'atendimento.horario'){
-      response.json({"fulfillmentText": "Horario de atendimento"});
+
+      response.json({
+        "fullfilmentMessages" : [{
+            "card": {
+              "title" : "Processo seletivo",
+              "subtitle" : "Bem-vindo ao nosso processo seletivo",
+              "imgUrl" : "https://tecnoblog.net/wp-content/uploads/2018/07/bot-twitter-700x394.jpg",
+              // "buttons" : [
+              //   {
+              //     "text" : "button text",
+              //     "postback" : "https://assistant.google.com/"
+              //   }
+              // ]
+            }
+          },
+          {
+            "text" : {
+              "text" : [
+                "Temos os melhores cursos nas areas de Humanas"
+              ]
+            }
+          }
+        ]
+      });
+
+
+
      }
    }
  }
