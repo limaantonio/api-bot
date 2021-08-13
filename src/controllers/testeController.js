b7406c8d0838797f8a4783d25d79a2f95a96d1a8
carlos <antonio.carlos@morgancs.com.br>
2021-08-12 22:34:17 -0300

    atualizar teste com intentname

--- src/controllers/testeController.js
+++ src/controllers/testeController.js
@@ -15,81 +15,81 @@ module.exports = {
      return response.status(200).json(action);
    },
 
-   fulfillmentText(request, response){
-     var intentName = request.body.queryResult.intent.displayName;
-
-     if(intentName === 'processo.seletivo'){
-      response.json({"fulfillmentText": "Processo seletivo"});
-     }else if(intentName === 'atendimento.horario'){
-      response.json({"fulfillmentText": "Horario de atendimento"});
-     }
-   }
-
-  //   async fulfillmentText(request, response){
+  //  fulfillmentText(request, response){
   //    var intentName = request.body.queryResult.intent.displayName;
 
   //    if(intentName === 'processo.seletivo'){
-
-  //     response.json({
-  //       "fullfilmentMessages" : [{
-  //           "card": {
-  //             "title" : "Processo seletivo",
-  //             "subtitle" : "Bem-vindo ao nosso processo seletivo",
-  //             "imgUrl" : "https://tecnoblog.net/wp-content/uploads/2018/07/bot-twitter-700x394.jpg",
-  //             // "buttons" : [
-  //             //   {
-  //             //     "text" : "button text",
-  //             //     "postback" : "https://assistant.google.com/"
-  //             //   }
-  //             // ]
-  //           }
-  //         },
-  //         {
-  //           "text" : {
-  //             "text" : [
-  //               "Temos os melhores cursos nas areas de Humanas"
-  //             ]
-  //           }
-  //         },
-  //         {
-  //           "text" : {
-  //             "text": [
-  //               "Voce quer participar do processo seletivo?"
-  //             ]
-  //           }
-  //         }
-  //       ]
-  //     });
-  //    }
-  //    else if(intentName === 'processo.seletivo-yes'){
-
-  //     var aluno_cep = request.body.queryResult.parameters['aluno-cep'];
-  //     var aluno_nome;
-  //     var aluno_cpf;
-  //     var aluno_curso; 
-
-
-  //     buscaCep(aluno_cep, {sync: false, timeout: 1000})
-  //     .then(endereco => {
-
-  //        aluno_nome = request.body.queryResult.parameters['aluno-cep'];
-  //        aluno_cpf = request.body.queryResult.parameters['aluno-cpf'];
-  //        aluno_curso = request.body.queryResult.parameters['aluno-curso'];
-
-  //       var endereco = endereco.logradoro+"-"+endereco.bairro+","+endereco.localidade+"-"+endereco.uf+"--"+endereco.cep;
-       
-  //     });
-
-  //     const aluno = {
-  //       aluno_nome ,
-  //       aluno_cpf,
-  //       aluno_curso,
-  //       endereco
-  //     }
-
-  //     await Alunos.create(aluno);
-
-  //     return response.json("fullfillmentText", "Voce foi cadastrado no nosso processo seletivo");
+  //     response.json({"fulfillmentText": "Processo seletivo"});
+  //    }else if(intentName === 'atendimento.horario'){
+  //     response.json({"fulfillmentText": "Horario de atendimento"});
   //    }
   //  }
+
+    async fulfillmentText(request, response){
+     var intentName = request.body.queryResult.intent.displayName;
+
+     if(intentName === 'processo.seletivo'){
+
+      response.json({
+        "fulfillmentMessages" : [{
+            "card": {
+              "title" : "Processo seletivo",
+              "subtitle" : "Bem-vindo ao nosso processo seletivo",
+              "imgUrl" : "https://tecnoblog.net/wp-content/uploads/2018/07/bot-twitter-700x394.jpg",
+              // "buttons" : [
+              //   {
+              //     "text" : "button text",
+              //     "postback" : "https://assistant.google.com/"
+              //   }
+              // ]
+            }
+          },
+          {
+            "text" : {
+              "text" : [
+                "Temos os melhores cursos nas areas de Humanas"
+              ]
+            }
+          },
+          {
+            "text" : {
+              "text": [
+                "Voce quer participar do processo seletivo?"
+              ]
+            }
+          }
+        ]
+      });
+     }
+     else if(intentName === 'processo.seletivo-yes'){
+
+      var aluno_cep = request.body.queryResult.parameters['aluno-cep'];
+      var aluno_nome;
+      var aluno_cpf;
+      var aluno_curso; 
+
+
+      buscaCep(aluno_cep, {sync: false, timeout: 1000})
+      .then(endereco => {
+
+         aluno_nome = request.body.queryResult.parameters['aluno-cep'];
+         aluno_cpf = request.body.queryResult.parameters['aluno-cpf'];
+         aluno_curso = request.body.queryResult.parameters['aluno-curso'];
+
+        var endereco = endereco.logradoro+"-"+endereco.bairro+","+endereco.localidade+"-"+endereco.uf+"--"+endereco.cep;
+       
+      });
+
+      const aluno = {
+        aluno_nome ,
+        aluno_cpf,
+        aluno_curso,
+        endereco
+      }
+
+      await Alunos.create(aluno);
+
+      return response.json("fullfillmentText", "Voce foi cadastrado no nosso processo seletivo");
+     }
+   }
  }
