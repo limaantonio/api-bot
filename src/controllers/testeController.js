6594e06304b76fb7d15e53f1aef44e2e704f62ba
carlos <antonio.carlos@morgancs.com.br>
2021-08-16 22:38:23 -0300

    atualizar teste com intentname

--- src/controllers/testeController.js
+++ src/controllers/testeController.js
@@ -26,59 +26,59 @@ module.exports = {
   //  }
 
     async fulfillmentText(request, response){
-    //  var intentName = request.body.queryResult.intent.displayName;
+     var intentName = request.body.queryResult.intent.displayName;
 
-    //  if(intentName === 'processo.seletivo'){
+     if(intentName === 'processo.seletivo'){
 
-    //   response.json({
-    //     "fulfillmentMessages" : [{
-    //         "card": {
-    //           "title" : "Processo seletivo",
-    //           "subtitle" : "Bem-vindo ao nosso processo seletivo",
-    //           "imgUrl" : "https://tecnoblog.net/wp-content/uploads/2018/07/bot-twitter-700x394.jpg",
-    //           // "buttons" : [
-    //           //   {
-    //           //     "text" : "button text",
-    //           //     "postback" : "https://assistant.google.com/"
-    //           //   }
-    //           // ]
-    //         }
-    //       },
-    //       {
-    //         "text" : {
-    //           "text" : [
-    //             "Temos os melhores cursos nas areas de Humanas"
-    //           ]
-    //         }
-    //       },
-    //       {
-    //         "text" : {
-    //           "text": [
-    //             "Voce quer participar do processo seletivo?"
-    //           ]
-    //         }
-    //       }
-    //     ]
-    //   });
-    //  }
-    //  else if(intentName === 'processo.seletivo-yes'){
+      response.json({
+        "fulfillmentMessages" : [{
+            "card": {
+              "title" : "Processo seletivo",
+              "subtitle" : "Bem-vindo ao nosso processo seletivo",
+              "imgUrl" : "https://tecnoblog.net/wp-content/uploads/2018/07/bot-twitter-700x394.jpg",
+              // "buttons" : [
+              //   {
+              //     "text" : "button text",
+              //     "postback" : "https://assistant.google.com/"
+              //   }
+              // ]
+            }
+          },
+          {
+            "text" : {
+              "text" : [
+                "Temos os melhores cursos nas areas de Humanas"
+              ]
+            }
+          },
+          {
+            "text" : {
+              "text": [
+                "Voce quer participar do processo seletivo?"
+              ]
+            }
+          }
+        ]
+      });
+     }
+     else if(intentName === 'processo.seletivo-yes'){
 
-    //   var aluno_cep = request.body.queryResult.parameters['aluno-cep'];
-    //   var aluno_nome;
-    //   var aluno_cpf;
-    //   var aluno_curso; 
+      var aluno_cep = request.body.queryResult.parameters['aluno-cep'];
+      var aluno_nome;
+      var aluno_cpf;
+      var aluno_curso; 
 
 
-    //   buscaCep(aluno_cep, {sync: false, timeout: 1000})
-    //   .then(endereco => {
+      buscaCep(aluno_cep, {sync: false, timeout: 1000})
+      .then(endereco => {
 
-    //      aluno_nome = request.body.queryResult.parameters['aluno-cep'];
-    //      aluno_cpf = request.body.queryResult.parameters['aluno-cpf'];
-    //      aluno_curso = request.body.queryResult.parameters['aluno-curso'];
+         aluno_nome = request.body.queryResult.parameters['aluno-cep'];
+         aluno_cpf = request.body.queryResult.parameters['aluno-cpf'];
+         aluno_curso = request.body.queryResult.parameters['aluno-curso'];
 
-    //     var endereco = endereco.logradoro+"-"+endereco.bairro+","+endereco.localidade+"-"+endereco.uf+"--"+endereco.cep;
+        var endereco = endereco.logradoro+"-"+endereco.bairro+","+endereco.localidade+"-"+endereco.uf+"--"+endereco.cep;
        
-    //   });
+      });
 
       const aluno = {
         aluno_nome: 'ac' ,
@@ -94,6 +94,6 @@ module.exports = {
         return response.status(500).json(err);
       }
      
-     
+    }
    }
  }
