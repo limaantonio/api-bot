5c65612ddb4b52a8384eedfe71da2dfe1921dc3f
carlos <antonio.carlos@morgancs.com.br>
2021-08-22 17:39:07 -0300

    atualizando onboarding

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -18,13 +18,21 @@ module.exports = {
     
       try {
         await Alunos.create(aluno);
-          response.json(
-            { 
-              "fulfillmentText" : "Seus dados foram salvos! Podemos continuar nossa conversa."
-              + "Como posso te ajudar?"
-              + "1 - Revisão"
-              + "2 - Orientação"
-              + "3 - Seus resultados"
+          response.json (
+            {
+              "fulfillmentMessages": [
+                {
+                  "text": {
+                    "text": [
+                      "Obrigado! Podemos continuar nossa conversa.",
+                      "Como posso te ajudar?",
+                      "1 - Revisão",
+                      "2 - Orientação",
+                      "3 - Seus resultados"
+                    ],
+                  }
+                }
+              ]
             }
           );
       } catch (error) {
