016a80933b4e247882971077e3c1c222b9a0bacf
carlos <antonio.carlos@morgancs.com.br>
2021-09-02 00:31:18 -0300

    add revisao.quiz - yes

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -80,7 +80,7 @@ module.exports = {
     }
     
     if (intentName === 'revisao.quiz - yes') {
-      let conteudo = request.body.queryResult.outputContexts[1].parameter['revisao-conteudo'];
+      let conteudo = request.body.queryResult.outputContexts[4].parameter['revisao-conteudo'];
       
       response.json (
         {
