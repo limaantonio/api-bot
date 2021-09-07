72960aa15dfbe6c6fb3e0eb10babfe82ed2ee7d0
carlos <antonio.carlos@morgancs.com.br>
2021-09-06 23:38:49 -0300

    ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
   entities: ["dist/models/**/*.js"],
   migrations: ["dist/database/migrations/**/*.js"],
   cli: {
-    migrationsDir: "src/database/migrations/",
+    migrationsDir: ["src/database/migrations/"],
+    entitiesDir: "src/models",
   },
 };
