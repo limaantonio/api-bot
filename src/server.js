62ce30c909978da6789d4cf38102facaa1664316
carlos <antonio.carlos@morgancs.com.br>
2021-09-06 22:38:18 -0300

    script build

--- src/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var listerner = app_1.app.listen(process.env.PORT || 3000, function () {
-    console.log("Server is running in port: " + listerner.address().port);
-});
